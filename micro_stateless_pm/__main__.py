0366d60f856aa48e36e1e52f096eebfedf130120
Stanislaw Adaszewski <s.adaszewski@gmail.com>
2024-04-14 14:13:33 +0200

    Add support for pyperclip.

--- micro_stateless_pm/__main__.py
+++ micro_stateless_pm/__main__.py
@@ -26,11 +26,21 @@ def main():
     try:
         root.tk.eval(f'tk::PlaceWindow {root._w} center')
         master_password = tkinter.simpledialog.askstring("Password", "Enter password:", show='*', parent=root)
+        root.destroy()
     except:
         master_password = getpass.getpass('Master Password: ')
     password_profile = create_profile(args)
     password = generate_password(password_profile, master_password)
-    print(password)
+    try:
+        import pyperclip
+    except:
+        print(password)
+        return
+    pyperclip.copy(password)
+    print('Password copied to clipboard. One you are done, press ENTER to clear the clipboard')
+    input()
+    pyperclip.copy('')
+
 
 
 if __name__ == '__main__':
