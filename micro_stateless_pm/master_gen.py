ff019e15644500bfea9ff7013219b94f29b00c10
Stanislaw Adaszewski <s.adaszewski@gmail.com>
2025-04-30 22:27:00 +0200

    Add master_gen.

--- micro_stateless_pm/master_gen.py
+++ micro_stateless_pm/master_gen.py
@@ -4,6 +4,7 @@ import math
 import os
 import unicodedata
 from .__main__ import clear_screen
+from .password import CHARACTER_SUBSETS
 
 
 def create_parser():
@@ -11,6 +12,8 @@ def create_parser():
     parser.add_argument('--number-of-words', '-n', type=int, default=3)
     parser.add_argument('--dictionary', '-d', type=str, required=True, help='Recommended dictionary: https://sjp.pl/sl/odmiany/')
     parser.add_argument('--compute-entropy-without-diacritics', '-c', action='store_true')
+    parser.add_argument('--add-number', '-a', action='store_true')
+    parser.add_argument('--add-symbol', '-s', action='store_true')
     return parser
 
 
@@ -29,27 +32,44 @@ def strip_diacritics(s):
     return res
 
 
+def random_int():
+    return int(''.join([ '%02x' % x for x in os.urandom(32) ]), 16) 
+
+
+def compute_entropy(word_selection, args):
+    entropy = math.log2(len(word_selection)) * args.number_of_words
+    if args.add_symbol:
+        entropy += math.log2(len(CHARACTER_SUBSETS['symbols']))
+    if args.add_number:
+        entropy += math.log2(2 * 9)
+    return entropy
+
+
 def main():
     parser = create_parser()
     args = parser.parse_args()
     word_selection = load_words(args.dictionary)
     print('Size of word selection:', len(word_selection))
-    entropy = math.log2(len(word_selection)) * args.number_of_words
-    print('Entropy:', entropy)
+    print('Entropy:', compute_entropy(word_selection, args))
     if args.compute_entropy_without_diacritics:
         word_selection_no_diacritics = set([ strip_diacritics(w) for w in word_selection ])
         print('Size of word selection (no diacritics):', len(word_selection_no_diacritics))
-        entropy = math.log2(len(word_selection_no_diacritics)) * args.number_of_words
-        print('Entropy (no diacritics):', entropy)
+        print('Entropy (no diacritics):', compute_entropy(word_selection_no_diacritics, args))
     word_selection = list(word_selection)
     # print(word_selection[:4])
     res = []
     for _ in range(args.number_of_words):
-        random_index = int(''.join([ '%02x' % x for x in os.urandom(32) ]), 16) % len(word_selection)
+        random_index = random_int() % len(word_selection)
         res.append(word_selection[random_index])
-    res = ' '.join(res)
-    print('Result:', res)
-    print('Without diacritics:', strip_diacritics(res))
+    if args.add_symbol:
+        res.append(CHARACTER_SUBSETS['symbols'][random_int() % len(CHARACTER_SUBSETS['symbols'])])
+    if args.add_number:
+        for _ in range(2):
+            res.append(str(1 + random_int() % 9))
+    # res = ' '.join(res)
+    print('Result:', ' '.join(res))
+    print('Without diacritics:', strip_diacritics(' '.join(res)))
+    print('Without spaces:', strip_diacritics(''.join(res)))
     input()
     clear_screen()
 
